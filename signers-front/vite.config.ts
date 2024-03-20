40165426dfd3f45bd5e91c1de528b1dc68574740
paulohfsantos <paulohfsantos1@gmail.com>
2024-03-19 23:58:23 -0300

    chore: fix hot-reload on frontend

--- signers-front/vite.config.ts
+++ signers-front/vite.config.ts
@@ -1,7 +1,30 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import tailwind from "tailwindcss"
+import autoprefixer from "autoprefixer"
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // add dev server options
+  server: {
+    watch: {
+      usePolling: true,
+    },
+
+    hmr: true,
+    port: 80,
+  },
 })
